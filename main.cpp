3f33f68fcba6c022fdaad3e3117faf0ecbd96c85
Rana Wahid <rana20479@feng.bu.edu.eg>
2023-11-03 21:15:48 +0200

    Creating getAvg function

--- main.cpp
+++ main.cpp
@@ -10,8 +10,14 @@ int getSum(vector<int>& numbers){
     return vectorSum;
 }
 
+float getAverage(vector<int>& numbers){
+    float sum= getSum(numbers);
+    return (float)(sum/numbers.size());
+}
+
 int main() {
     vector<int>numbers={1,2,3,4,5,6,7,8,9,10};
     cout<<"Sum of the vector \"numbers\" is : "<<getSum(numbers)<<'\n';
+    cout<<"Average of the vector \"numbers\" is : "<<getAverage(numbers)<<'\n';
     return 0;
 }

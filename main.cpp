bb7aba6e9ff666e481803b02fe38f28946eb9594
Rana Wahid <rana20479@feng.bu.edu.eg>
2023-11-03 21:18:21 +0200

    Creating getMin Function

--- main.cpp
+++ main.cpp
@@ -14,10 +14,18 @@ float getAverage(vector<int>& numbers){
     float sum= getSum(numbers);
     return (float)(sum/numbers.size());
 }
+int getMin(vector<int>& numbers){
+    int minNumber=INT_MAX;
+    for(auto i:numbers){
+        minNumber=min(minNumber,i);
+    }
+    return minNumber;
+}
 
 int main() {
     vector<int>numbers={1,2,3,4,5,6,7,8,9,10};
     cout<<"Sum of the vector \"numbers\" is : "<<getSum(numbers)<<'\n';
     cout<<"Average of the vector \"numbers\" is : "<<getAverage(numbers)<<'\n';
+    cout<<"Minimum Number in the vector \"numbers\" is : "<<getMin(numbers)<<'\n';
     return 0;
 }
